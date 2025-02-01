5122aeea2dd09466d9c8c3b0adfaf796c475c6b7
Phillip Williams <pcaw627@gmail.com>
2025-01-31 22:49:20 -0500

    Moved all files into same directory for Gradescope grading

--- alu-main/full_adder.v
+++ alu-main/full_adder.v
@@ -1,14 +1,14 @@
-module full_adder(S, Cout, A, B, Cin);
-    input A, B, Cin;
-    output S, Cout;
+module full_adder(out1, out2, in1, in2, in3);
+    input in1, in2, in3;
+    output out1, out2;
     wire w1, w2, w3;
 
     // syntax: gate instance_name(output, input_1, input_2...)
-    xor Sresult(S, A, B, Cin);
-    and A_and_B(w1, A, B);
-    and A_and_C(w2, A, Cin);
-    and B_and_C(w3, B, Cin);
-    or (Cout, w1, w2, w3);
+    xor Sresult(out1, in1, in2, in3);
+    and A_and_B(w1, in1, in2);
+    and A_and_C(w2, in1, in3);
+    and B_and_C(w3, in2, in3);
+    or Cout(out2, w1, w2, w3);
 
 endmodule
 

dec3923a2877f6bc6a993a6f106f5f7b9e4a2300
Phillip Williams <pcaw627@gmail.com>
2025-02-01 00:11:32 -0500

    Removed files not matching .gitignore conditions.

--- alu-main/comp_mux_2.v
+++
@@ -1,9 +0,0 @@
-
-module comp_mux_2(out, select, in0, in1);
-    input select;
-    input in0, in1;
-    output out;
-
-    assign out = select ? in1 : in0;
-
-endmodule
--- alu-main/sll_32_16b.v
+++
@@ -1,8 +0,0 @@
-module sll_32_16b (
-    input [31:0] in,
-    output [31:0] out
-);
-
-    assign out[15:0] = 16'b0000000000000000;
-    assign out[31:16] = in[15:0];
-endmodule

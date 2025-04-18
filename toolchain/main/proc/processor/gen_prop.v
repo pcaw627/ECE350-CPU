5be3281b764e08b9bedecc48bda7c5bd211f4013
pcaw627 <pcaw627@gmail.com>
2025-04-17 21:40:03 -0400

    Reset processor folder for testing

--- toolchain/main/proc/processor/gen_prop.v
+++
@@ -1,9 +0,0 @@
-module gen_prop(g, p, a, b);
-
-    input a, b;
-    output g, p;
-
-    and and1(g, a, b);  // Creates g term (a*b)
-    xor xor1(p, a, b);  // Creates p term xor(a,b)
- 
-endmodule
